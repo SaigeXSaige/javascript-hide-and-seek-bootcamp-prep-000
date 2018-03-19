b47379b3ccb7e46f162d100a86e2fe8f4d780bb5
Saige Leslie <saigeleslie@gmail.com>
2018-03-19 18:31:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ function deepestChild(){
 }
 function increaseRankBy(n){
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
-  const firstList = rankedLists[0];
-  const secondList = rankedLists[1];
   
-  
+  for (let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = (i + n).toString()
+  }
 }
