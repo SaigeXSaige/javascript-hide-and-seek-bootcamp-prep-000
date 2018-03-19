f7dff31f3acc7ca50b01a32d1a62d4288704795d
Saige Leslie <saigeleslie@gmail.com>
2018-03-19 18:14:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ function deepestChild(){
   return document.querySelector('#grand-node div div div div');
 }
 function increaseRankBy(n){
-  
+  document.querySelectorAll('.ranked-list')
+  return 
 }
